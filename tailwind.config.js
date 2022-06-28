5a4ff5e98058cb0d4fead8cfff7979649ceabc0c
giorgitchanturidze <chanturidzegiorgi31@gmail.com>
2022-06-28 20:33:01 +0400

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html.twig,js,html}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        violet: "#5964e0",
+        light_violet: "#939bf4",
+        very_dark: "#19202d",
+        midnight: "#121721",
+        light_grey: {
+          50: "#f4f6f8",
+          100: "#eeeffc",
+          200: "#c5c9f4"
+        },
+        dark: "#303642",
+        light_dark: "#696e76",
+        grey: "#9daec2",
+        dark_grey: "#6e8098",
+      },
+    },
+  },
+  plugins: [],
+}
